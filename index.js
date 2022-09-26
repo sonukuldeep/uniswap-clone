31a52fa0fc64cb79791286bf709f200d989c17bf
sonukuldeep <sonukuldeep@outlook.com>
2022-09-26 12:36:54 +0530

    small fix

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 const setAction = (value) => {
         document.querySelector('.title').innerHTML = value;
-
-}
-
-const selectElement = document.querySelector('.rightNav > button');
-selectElement.addEventListener("mouseover", dropDownEnable);
-selectElement.addEventListener("mouseout", dropDownDisable)
-
-function dropDownEnable() {
-
-        document.querySelector('.rightNavDropDown').className = 'rightNavDropDownEnable';
-       
-        
         
 }
 
-function dropDownDisable() {
-        const disableFun = () => {
-                console.log('disable');
-        }
-        const callMe = ()=>{
-                document.querySelector('.rightNavDropDownEnable').addEventListener('mouseout', disableFun) // check if mouse over selectElement
-                //if not then disable it 
-                document.querySelector('.rightNavDropDownEnable').className = 'rightNavDropDown';
-                
-        }
-        c = setTimeout(callMe ,1000)
-}
+
+
